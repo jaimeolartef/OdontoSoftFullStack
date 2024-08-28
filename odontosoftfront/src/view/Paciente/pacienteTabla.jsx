4c5d6bd932981d96f0f7acddc9643a03370aea8c
Jaime Andrés Alberto Olarte Fuentes <jaimeolartef@gmail.com>
2024-08-27 19:42:45 -0500

    Se agregan los estilos de la tabla

--- odontosoftfront/src/view/Paciente/pacienteTabla.jsx
+++ odontosoftfront/src/view/Paciente/pacienteTabla.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './pacienteTabla.css';
 
 const PacienteTabla = ({ data }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  // Calculate the indices for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Calculate total pages
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  // Handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="table-container">
       <table>
@@ -15,7 +31,7 @@ const PacienteTabla = ({ data }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((paciente, index) => (
+          {currentItems.map((paciente, index) => (
             <tr key={index}>
               <td data-label="Documento">{paciente.documento}</td>
               <td data-label="Nombre">{paciente.primernombre} {paciente.segundonombre}</td>
@@ -26,6 +42,17 @@ const PacienteTabla = ({ data }) => {
           ))}
         </tbody>
       </table>
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? 'active' : ''}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
