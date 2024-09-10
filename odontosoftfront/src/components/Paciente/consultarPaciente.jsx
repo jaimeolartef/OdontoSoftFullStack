1f8e97b5ff59b77cd5e9748b90cbbc32480b99bb
Jaime Andrés Alberto Olarte Fuentes <jaimeolartef@gmail.com>
2024-09-09 19:03:10 -0500

    se cambio el estilo de la pantalla tablapaciente.jsx

--- odontosoftfront/src/components/Paciente/consultarPaciente.jsx
+++ odontosoftfront/src/components/Paciente/consultarPaciente.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './consultarPaciente.css';
 import Logo from '../../resource/LogoNegro.png';
 import '../../App.css';
 import axios from "axios";
 import config from "../../config";
 import PacienteTabla from "./pacienteTabla";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import showMessage from "../../util/UtilMessage";
 
 const ConsultarPaciente = () => {
@@ -22,16 +23,13 @@ const ConsultarPaciente = () => {
     if (redireccionadoModificar) {
       if (localStorage.getItem('consultarPacienteFormData')) {
         setFormData(JSON.parse(localStorage.getItem('consultarPacienteFormData')));
-        console.log('Se va a consumir el servicio con:', formData);
         fetchPacientes(JSON.parse(localStorage.getItem('consultarPacienteFormData'))).then(r => r);
-        console.log('Removing item from localStorage : ' + localStorage.getItem('consultarPacienteFormData'));
         localStorage.removeItem('consultarPacienteFormData');
-        console.log('Item removed from localStorage' + localStorage.getItem('consultarPacienteFormData'));
       }
     } else if (localStorage.getItem('consultarPacienteFormData')) {
-        localStorage.removeItem('consultarPacienteFormData');
+      localStorage.removeItem('consultarPacienteFormData');
     }
-}, []);
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -45,65 +43,69 @@ const ConsultarPaciente = () => {
     e.preventDefault();
 
     if (!formData.documento && !formData.nombre) {
-      showMessage('warning','Debe realizar la busqueda por nombre o por documento.');
+      showMessage('warning', 'Debe realizar la busqueda por nombre o por documento.');
       return;
     }
     fetchPacientes(formData).then(r => r);
   };
 
-const fetchPacientes = async (formData) => {
-  try {
-    let token = localStorage.getItem('jsonwebtoken');
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    const response = await axios.get(`${config.baseURL}/pacientes/consultar`, {
-      params: formData,
-      validateStatus: function (status) {
-        return status;
-      }
-    });
+  const fetchPacientes = async (formData) => {
+    try {
+      let token = localStorage.getItem('jsonwebtoken');
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      const response = await axios.get(`${config.baseURL}/pacientes/consultar`, {
+        params: formData,
+        validateStatus: function (status) {
+          return status;
+        }
+      });
 
-    if (response.status === 200) {
-      setResponseData(response.data);
-    } else if (response.status === 400 && response.data.codigoValidacion === '400') {
-      showMessage('error',response.data.mensajeValidacion);
-    } else if (response.status === 403) {
-      showMessage('error','No autorizado');
-    } else if (response.status > 400) {
-      showMessage('error','Error en la solicitud');
+      if (response.status === 200) {
+        setResponseData(response.data);
+      } else if (response.status === 400 && response.data.codigoValidacion === '400') {
+        showMessage('error', response.data.mensajeValidacion);
+      } else if (response.status === 403) {
+        showMessage('error', 'No autorizado');
+      } else if (response.status > 400) {
+        showMessage('error', 'Error en la solicitud');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      if (error.response && error.response.status === 403) {
+        showMessage('error', 'No autorizado');
+      } else {
+        showMessage('error', 'Error en la solicitud');
+      }
     }
-  } catch (error) {
-    console.error('Error:', error);
-    if (error.response && error.response.status === 403) {
-      showMessage('error','No autorizado');
-    } else {
-      showMessage('error','Error en la solicitud');
-    }
-  }
-};
+  };
 
   return (
-    <div className="medical-form-container">
-      <header>
-        <img src={Logo} alt="Logo"/>
-        <h1>Registrar Paciente</h1>
-      </header>
-      <form onSubmit={handleSubmit}>
-        <section className="personal-information">
-          <h3>Consultar paciente</h3>
-          <div className="input-box">
-            <input type="text" name="documento" value={formData.documento} onChange={handleChange}/>
-            <label>Número de documento de identidad</label>
+    <div className="d-flex justify-content-center align-items-center ">
+      <div className="card p-4" style={{ width: '800px' }}>
+        <header className="text-center mb-4">
+          <img src={Logo} alt="Logo" className="mb-3" style={{ maxWidth: '140px' }} />
+          <h1>Consultar Paciente</h1>
+        </header>
+        <form onSubmit={handleSubmit} className="needs-validation" noValidate>
+          <section className="mb-4">
+            <h3>Información Personal</h3>
+            <div className="form-floating mb-3">
+              <input type="text" className="form-control" name="documento" value={formData.documento} onChange={handleChange} placeholder="Documento de Identidad" />
+              <label>Documento de Identidad</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input type="text" className="form-control" name="nombre" value={formData.nombre} onChange={handleChange} placeholder="Nombre" />
+              <label>Nombre</label>
+            </div>
+          </section>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">Consultar</button>
           </div>
-          <div className="input-box">
-            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
-            <label>Nombre</label>
-          </div>
-        </section>
-        <button type="submit" className="btn">Consultar</button>
-      </form>
-      <br/>
-      {responseData.length > 0 && <PacienteTabla data={responseData} formData={formData}/>}
-      <br/>
+        </form>
+        <br />
+        {responseData.length > 0 && <PacienteTabla data={responseData} formData={formData} />}
+        <br />
+      </div>
     </div>
   );
 }
--- odontosoftfront/src/components/Paciente/pacienteTabla.jsx
+++ odontosoftfront/src/components/Paciente/pacienteTabla.jsx
@@ -1,100 +1,108 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './pacienteTabla.css';
-import MedicalIcon from '../../resource/MedicalIcon.png'; // Importar el icono desde la carpeta resource
-import EditIcon from '../../resource/EditIcon.png'; // Importar el icono desde la carpeta resource
-import {Tooltip} from 'react-tooltip'; // Importar el componente Tooltip
-import bootstrap from 'bootstrap'; // Importar bootstrap
+import MedicalIcon from '../../resource/MedicalIcon.png';
+import EditIcon from '../../resource/EditIcon.png';
+import { Tooltip } from 'react-tooltip';
 
-const PacienteTabla = ({data, formData}) => {
+const PacienteTabla = ({ data, formData }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
   const navigate = useNavigate();
 
-  // Calculate the indices for the current page
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
-  // Calculate total pages
   const totalPages = Math.ceil(data.length / itemsPerPage);
 
-  // Handle page change
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  // Handle row click
   const handleEditClick = (paciente) => {
     localStorage.setItem('consultarPacienteFormData', JSON.stringify(formData));
-    navigate('/modificarPac', {state: {id: paciente.id}});
+    navigate('/modificarPac', { state: { id: paciente.id } });
   };
 
-  // Handle medical record icon click
   const handleMedicalRecordClick = (paciente) => {
-    navigate('/historiaPac', {state: {idPatient: paciente.id}});
+    navigate('/historiaPac', { state: { idPatient: paciente.id } });
   };
-
 
   return (
     <div className="table-container">
       <table className="table table-striped table-bordered">
         <thead className="thead-dark">
-        <tr>
-          <th>Documento</th>
-          <th>Nombre</th>
-          <th>Primer Apellido</th>
-          <th>Segundo Apellido</th>
-          <th>Teléfono</th>
-          <th>Habilitado</th>
-          <th>Acciones</th>
-        </tr>
+          <tr>
+            <th>Documento</th>
+            <th>Nombre</th>
+            <th>Primer Apellido</th>
+            <th>Segundo Apellido</th>
+            <th>Teléfono</th>
+            <th>Habilitado</th>
+            <th>Acciones</th>
+          </tr>
         </thead>
         <tbody>
-        {currentItems.map((paciente, index) => (
-          <tr key={index}>
-            <td data-label="Documento">{paciente.documento}</td>
-            <td data-label="Nombre">{paciente.primernombre} {paciente.segundonombre}</td>
-            <td data-label="Primer Apellido">{paciente.primerapellido}</td>
-            <td data-label="Segundo Apellido">{paciente.segundoapellido}</td>
-            <td data-label="Teléfono">{paciente.telefono}</td>
-            <td data-label="Habilitado">{paciente.habilitado === 'true' ? 'Sí' : 'No'}</td>
-            <td data-label="Acciones">
-              {paciente.habilitado === 'true' && (
-                <>
-                  <img src={MedicalIcon} alt="Historia Clinica"
-                       style={{marginRight: '5px', width: '35px', height: '35px', cursor: 'pointer'}}
-                       onClick={(e) => {
-                         e.stopPropagation();
-                         handleMedicalRecordClick(paciente);
-                       }}
-                       data-tooltip-id="tooltip" data-tooltip-content="Ver Historia Clinica"/>
-                  <Tooltip id="tooltip"/>
-                </>
-              )}
-              <img src={EditIcon} alt="Editar Paciente"
-                   style={{marginRight: '5px', width: '35px', height: '35px', cursor: 'pointer'}}
-                   onClick={(e) => {
-                     e.stopPropagation();
-                     handleEditClick(paciente);
-                   }}
-                   data-tooltip-id="editTooltip" data-tooltip-content="Editar Paciente"/>
-              <Tooltip id="editTooltip"/>
-            </td>
-          </tr>
-        ))}
+          {currentItems.map((paciente, index) => (
+            <tr key={index}>
+              <td data-label="Documento">{paciente.documento}</td>
+              <td data-label="Nombre">{paciente.primernombre} {paciente.segundonombre}</td>
+              <td data-label="Primer Apellido">{paciente.primerapellido}</td>
+              <td data-label="Segundo Apellido">{paciente.segundoapellido}</td>
+              <td data-label="Teléfono">{paciente.telefono}</td>
+              <td data-label="Habilitado">{paciente.habilitado === 'true' ? 'Sí' : 'No'}</td>
+              <td data-label="Acciones">
+                {paciente.habilitado === 'true' && (
+                  <>
+                    <img src={MedicalIcon} alt="Historia Clinica"
+                      style={{ marginRight: '5px', width: '35px', height: '35px', cursor: 'pointer' }}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleMedicalRecordClick(paciente);
+                      }}
+                      data-tooltip-id="tooltip" data-tooltip-content="Ver Historia Clinica" />
+                    <Tooltip id="tooltip" />
+                  </>
+                )}
+                <img src={EditIcon} alt="Editar Paciente"
+                  style={{ marginRight: '5px', width: '35px', height: '35px', cursor: 'pointer' }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleEditClick(paciente);
+                  }}
+                  data-tooltip-id="editTooltip" data-tooltip-content="Editar Paciente" />
+                <Tooltip id="editTooltip" />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      <ul className="pagination">
-        {Array.from({length: totalPages}, (_, index) => (
-          <li key={index + 1} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
-            <button
-              onClick={() => handlePageChange(index + 1)}>
-              {index + 1}
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`} style={{width: '110px'}}>
+            <button className="page-link" onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}>
+              Anterior
             </button>
           </li>
-        ))}
-      </ul>
+          {Array.from({length: totalPages}, (_, index) => (
+            <li key={index + 1} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+              <button className="page-link" onClick={() => handlePageChange(index + 1)}
+                      disabled={currentPage === totalPages}>
+                {index + 1}
+              </button>
+            </li>
+          ))}
+          <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`} style={{width: '110px'}}>
+            <button className="page-link" onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}>
+              Siguiente
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
