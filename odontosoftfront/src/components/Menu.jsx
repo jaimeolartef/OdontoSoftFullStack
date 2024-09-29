c3f04d873884a9293aa219f59bea75ca7509ede0
Jaime Andrés Alberto Olarte Fuentes <jaimeolartef@gmail.com>
2024-09-28 20:09:27 -0500

    Se construyen la pantalla historia clinica

--- odontosoftfront/src/components/Menu.jsx
+++ odontosoftfront/src/components/Menu.jsx
@@ -1,59 +1,45 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import { connect } from "react-redux";
-import './Menu.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Menu(props) {
-  // Recuperar la variable del localStorage
   let data = localStorage.getItem('menuUser');
-  let dataMenu = [];
-
-  // Comprobar si la variable existe
-  if (data) {
-    // Convertir la cadena JSON en un objeto JavaScript
-    data = JSON.parse(data);
-
-    // Recorrer el objeto
-    for (let key in data) {
-      if (data[key] != null) {
-        dataMenu.push(JSON.parse(JSON.stringify(data[key])));
-      }
-    }
-  } else {
-    console.log('No data found in localStorage for key "menuUser"');
-  }
+  let dataMenu = data ? JSON.parse(data) : [];
 
   const renderSubMenu = (menu) => {
-    if (menu.menuHijo !== undefined && menu.menuHijo !== null && menu.menuHijo.length > 0) {
-      return (
-        <div className="dropdown-content">
-          {menu.menuHijo.map((subMenu, index) => (
-            <Link key={index} to={subMenu.url}>{subMenu.nombreMenu}</Link>
-          ))}
-        </div>
-      );
-    }
-    return null;
+    return (
+      <NavDropdown title={menu.nombreMenu} id={`dropdown-${menu.id}`}>
+        {menu.menuHijo && menu.menuHijo.map((subMenu, index) => (
+          <NavDropdown.Item key={index} as={Link} to={subMenu.url}>
+            {subMenu.nombreMenu}
+          </NavDropdown.Item>
+        ))}
+      </NavDropdown>
+    );
   };
 
   return (
-    <nav className="menu">
-      {props.isLoggedIn != null && props.isLoggedIn ?
-        dataMenu.map((menu, index) => (
-          <div key={index} className="menu-item">
-            <Link to={menu.url}>{menu.nombreMenu}</Link>
-            {renderSubMenu(menu)}
-          </div>
-        ))
-        : null}
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          {props.isLoggedIn && dataMenu.map((menu, index) => (
+            <React.Fragment key={index}>
+              {menu.menuHijo ? renderSubMenu(menu) : (
+                <Nav.Link as={Link} to={menu.url}>{menu.nombreMenu}</Nav.Link>
+              )}
+            </React.Fragment>
+          ))}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.isLoggedIn
-  }
-}
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.isLoggedIn
+});
 
 export default connect(mapStateToProps)(Menu);
