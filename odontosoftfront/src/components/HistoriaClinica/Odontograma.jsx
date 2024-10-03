16c283e5eaab899ba38598879281f6c343027f21
Jaime Andrés Alberto Olarte Fuentes <jaimeolartef@gmail.com>
2024-10-02 19:45:21 -0500

    :construction: Se construyen la pantalla historia clinica

--- odontosoftfront/src/components/HistoriaClinica/Odontograma.jsx
+++ odontosoftfront/src/components/HistoriaClinica/Odontograma.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './Odontograma.css'; // Asegúrate de crear un archivo CSS para estilos personalizados
 import CondicionesDentales from './CondicionesDentale';
 
@@ -26,16 +26,24 @@ const Odontograma = () => {
   );
 
   return (
-    <div className="odontograma-container">
-      <CondicionesDentales />
-      <div className="odontograma">
-        <div className="row">
-          {Array.from({ length: 8 }, (_, i) => renderTooth(i, 18 - i))}
-          {Array.from({ length: 8 }, (_, i) => renderTooth(i + 8, 21 + i))}
-        </div>
-        <div className="row">
-          {Array.from({ length: 8 }, (_, i) => renderTooth(i + 16, 48 - i))}
-          {Array.from({ length: 8 }, (_, i) => renderTooth(i + 24, 31 + i))}
+    <div className="card">
+      <div className="card-header">
+        <h2>Análisis Funcional</h2>
+      </div>
+      <div className="card-body">
+        <div className="odontograma-container">
+          <CondicionesDentales/>
+          <div className="espacio"/>
+          <div className="odontograma">
+            <div className="row">
+              {Array.from({length: 8}, (_, i) => renderTooth(i, 18 - i))}
+              {Array.from({length: 8}, (_, i) => renderTooth(i + 8, 21 + i))}
+            </div>
+            <div className="row">
+              {Array.from({length: 8}, (_, i) => renderTooth(i + 16, 48 - i))}
+              {Array.from({length: 8}, (_, i) => renderTooth(i + 24, 31 + i))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
