eb2c340501ce3a7af89b33ae4a952a0094fc2b8d
Jaime Andrés Alberto Olarte Fuentes <jaimeolartef@gmail.com>
2024-10-02 22:03:41 -0500

    :construction: Se construyen la pantalla historia clinica

--- odontosoftfront/src/components/HistoriaClinica/CondicionesDentale.jsx
+++ odontosoftfront/src/components/HistoriaClinica/CondicionesDentale.jsx
@@ -5,55 +5,43 @@ const CondicionesDentales = () => {
   return (
     <div className="condiciones-dentales">
       <table>
-        <thead>
-        <tr>
-          <th>SIMB</th>
-          <th>CONDICIONES</th>
-          <th>COLOR</th>
-          <th>SIMB</th>
-          <th>CONDICIONES</th>
-          <th>COLOR</th>
-          <th>SIMB</th>
-          <th>CONDICIONES</th>
-          <th>COLOR</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td className="red-figure">■</td>
-          <td>Caries o recurrencia</td>
-          <td>Rojo</td>
-          <td className="blue-figure">■</td>
-          <td>Obturado</td>
-          <td>Azul</td>
-          <td className="blue-figure" style={{transform: 'scale(1.4)' }}>□</td>
-          <td>Corona completa</td>
-          <td>Azul</td>
-        </tr>
-        <tr>
-          <td className="blue-figure" style={{transform: 'scale(1.4)' }}>=</td>
-          <td>Prótesis existente</td>
-          <td>Azul</td>
-          <td className="red-figure">X</td>
-          <td>Extracción Indicada</td>
-          <td>Rojo</td>
-          <td className="blue-figure" style={{transform: 'scale(1.6)' }}>-</td>
-          <td>Sin erupcionar</td>
-          <td>Azul</td>
-        </tr>
-        <tr>
-          <td className="blue-figure">I</td>
-          <td>Extraído</td>
-          <td>Azul</td>
-          <td className="red-figure">Δ</td>
-          <td>Necesita endodoncia</td>
-          <td>Rojo</td>
-          <td className="blue-figure">Δ</td>
-          <td>Con tratamiento de conductos</td>
-          <td>Azul</td>
-        </tr>
-        </tbody>
-      </table>
+  <thead>
+  <tr>
+    <th>CONDICIONES</th>
+    <th>SIMB</th>
+    <th>CONDICIONES</th>
+    <th>SIMB</th>
+    <th>CONDICIONES</th>
+    <th>SIMB</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <td>Caries o recurrencia</td>
+    <td className="red-figure">■</td>
+    <td>Obturado</td>
+    <td className="blue-figure">■</td>
+    <td>Corona completa</td>
+    <td className="blue-figure" style={{transform: 'scale(1.4)' }}>□</td>
+  </tr>
+  <tr>
+    <td>Prótesis existente</td>
+    <td className="blue-figure" style={{transform: 'scale(1.4)' }}>=</td>
+    <td>Extracción Indicada</td>
+    <td className="red-figure">X</td>
+    <td>Sin erupcionar</td>
+    <td className="blue-figure" style={{transform: 'scale(1.6)' }}>-</td>
+  </tr>
+  <tr>
+    <td>Extraído</td>
+    <td className="blue-figure">I</td>
+    <td>Necesita endodoncia</td>
+    <td className="red-figure">Δ</td>
+    <td>Con tratamiento de conductos</td>
+    <td className="blue-figure">Δ</td>
+  </tr>
+  </tbody>
+</table>
     </div>
   );
 };
