05f5234725dee078a8c394ccc217874699f29b62
Jaime Andrés Alberto Olarte Fuentes <jaimeolartef@gmail.com>
2024-11-20 21:24:09 -0500

    feat(Cita Medica): :construction: Pantalla cita medica
    Se crea el back end de cita medica

--- OdontoSoftBack/src/main/java/org/enterprise/odontosoft/controller/mapper/AvailabilityMapper.java
+++ OdontoSoftBack/src/main/java/org/enterprise/odontosoft/controller/mapper/AvailabilityMapper.java
@@ -1,8 +1,10 @@
 package org.enterprise.odontosoft.controller.mapper;
 
+import lombok.experimental.UtilityClass;
 import org.enterprise.odontosoft.model.Entity.Disponibilidad;
 import org.enterprise.odontosoft.view.dto.response.DisponibilidadResponse;
 
+@UtilityClass
 public class AvailabilityMapper {
 
 	public static DisponibilidadResponse toAvailabilityResponse(Disponibilidad disponibilidad) {
